b48430b215f4d6f20fec3d91d693eac8e1af5fd0
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-11-23 02:05:55 +0300

    Commit uncommited

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import GithubUserFinder from "./components/GithubUserFinder";
+import Header from "./components/Header";
 
 function App() {
-  return;
+  return (
+    <div className="container">
+      <Header />
+      <GithubUserFinder />
+    </div>
+  );
 }
 
 export default App;
