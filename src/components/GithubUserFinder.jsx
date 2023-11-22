ed010285ebab72b563eedaf690f937d9c7e0f9a9
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-11-23 00:45:35 +0300

    Add functionalities

--- src/components/GithubUserFinder.jsx
+++ src/components/GithubUserFinder.jsx
@@ -1,9 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function GithubUserFinder() {
-  const [userName, setUserName] = useState("");
+  const [username, setUsername] = useState("");
   const [userData, setUserData] = useState(null);
-  const [Error, setError] = useState(null);
+  const [userRepos, setUserRepos] = useState([]);
+  const [error, setError] = useState(null);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.github.com/users/${username}`
+      );
+      setUserData(response.data);
+      setError(null);
+    } catch (error) {
+      setUserData(null);
+      setError("User not found");
+    }
+  };
+
+  useEffect(() => {
+    if (userData) {
+      fetchUserRepos();
+    }
+  }, [userData]);
+
+  const fetchUserRepos = async () => {
+    try {
+      const responce = await axios.get(
+        `https://api.github.com/users/${username}/repos`
+      );
+      setUserRepos(responce.data);
+      setError(null);
+    } catch (error) {
+      setUserRepos([]);
+      setError("No repositories found");
+    }
+  };
+
+  const handleClear = () => {
+    setUserData(null);
+    setUsername("");
+  };
 
   return (
     <div className="main-section">
@@ -13,10 +52,30 @@ function GithubUserFinder() {
       </div>
       <input
         type="text"
-        value={userName}
-        onChange={(e) => setUserName(e.target.value)}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
       <button onClick={handleSearch}>Search</button>
+      <button onClick={handleClear}>Clear</button>
+      {userData && (
+        <div>
+          <h3>{userData.name}</h3>
+          <p>{userData.bio}</p>
+          <p>Followers: {userData.followers}</p>
+          <p>Following: {userData.following}</p>
+          {userRepos.length > 0 && (
+            <div>
+              <h3>Repositories:</h3>
+              <ul>
+                {userRepos.map((repo) => (
+                  <li key={repo.id}>{repo.name}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      )}
+      {error && <p>{error}</p>}
     </div>
   );
 }
