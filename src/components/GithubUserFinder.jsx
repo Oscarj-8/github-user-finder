a31da28b75a9fa689c3afbaef7d6efb10187c6e5
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-11-23 01:49:45 +0300

    Extend amount of repos fetched

--- src/components/GithubUserFinder.jsx
+++ src/components/GithubUserFinder.jsx
@@ -29,7 +29,7 @@ function GithubUserFinder() {
   const fetchUserRepos = async () => {
     try {
       const responce = await axios.get(
-        `https://api.github.com/users/${username}/repos`
+        `https://api.github.com/users/${username}/repos?per_page=100`
       );
       setUserRepos(responce.data);
       setError(null);
@@ -46,29 +46,39 @@ function GithubUserFinder() {
 
   return (
     <div className="main-section">
-      <div>
+      <div className="main-top">
         <h2>Search GitHub Users</h2>
         <p>Enter GitHub username to search user profile:</p>
+        <div className="search-section">
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search</button>
+          <button onClick={handleClear}>Clear</button>
+        </div>
       </div>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <button onClick={handleSearch}>Search</button>
-      <button onClick={handleClear}>Clear</button>
+
       {userData && (
-        <div>
-          <h3>{userData.name}</h3>
-          <p>{userData.bio}</p>
-          <p>Followers: {userData.followers}</p>
-          <p>Following: {userData.following}</p>
+        <div className="userData-section">
+          <div>
+            <img src={userData.avatar_url} alt="User avatar" />
+            <h3>{userData.name}</h3>
+            <p>{userData.bio}</p>
+            <p>Followers: {userData.followers}</p>
+            <p>Following: {userData.following}</p>
+          </div>
+
           {userRepos.length > 0 && (
-            <div>
+            <div className="user-repos">
               <h3>Repositories:</h3>
               <ul>
-                {userRepos.map((repo) => (
-                  <li key={repo.id}>{repo.name}</li>
+                {userRepos.map((repo, index) => (
+                  <li key={repo.id}>
+                    {index + 1}
+                    {repo.name}
+                  </li>
                 ))}
               </ul>
             </div>
