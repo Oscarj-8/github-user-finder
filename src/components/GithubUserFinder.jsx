d1418628f4610d9733f8ba4d5288080255d7e631
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-11-22 23:39:13 +0300

    Add axios

--- src/components/GithubUserFinder.jsx
+++ src/components/GithubUserFinder.jsx
@@ -1,6 +1,23 @@
-import Header from "./Header";
+import { useState } from "react";
 
 function GithubUserFinder() {
-  return <div>GithubUserFinder</div>;
+  const [userName, setUserName] = useState("");
+  const [userData, setUserData] = useState(null);
+  const [Error, setError] = useState(null);
+
+  return (
+    <div className="main-section">
+      <div>
+        <h2>Search GitHub Users</h2>
+        <p>Enter GitHub username to search user profile:</p>
+      </div>
+      <input
+        type="text"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
 }
 export default GithubUserFinder;
